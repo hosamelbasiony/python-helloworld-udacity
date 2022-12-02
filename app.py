d79524c24f9d5c64a07a02fe5df584affaaa6f9a
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2022-12-02 17:10:29 +0200

    2222

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
+import logging
+
 from flask import Flask
 from flask import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-@app.route('/summary')
-def summary():
-    data = { "payload": "Hello There!" }
+@app.route('/status')
+def healthcheck():
     response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
     )
+
+    app.logger.info('Status request successfull')
     return response
 
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success", "code":0, "data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
+@app.route("/")
+def hello():
+    app.logger.info('Main request successfull')
+
+    return "Hello World!"
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    ## stream logs to a file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
+    app.run(host='0.0.0.0', port=8080)
